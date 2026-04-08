4a93dd7b99ebfa2a5fed8e970b738a387926e17d
steven L <steven.s.llamas@gmail.com>
2026-04-08 08:06:00 -0500

    finalized BasicMessageParser Unit Tests

--- src/test/java/hl7Viewer/nonGui/parser/BasicMessageParserTest.java
+++ src/test/java/hl7Viewer/nonGui/parser/BasicMessageParserTest.java
@@ -10,11 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BasicMessageParserTest {
     private BasicMessageParser parser;
-    private final String validHl7Message =
-            """
-                    MSH|^~\\\\&|HIS_System|Hospital_A|Lab_System|Lab_Dept|202603051000||ADT^A04|MSG00001|P|2.3
-                    PID|||PI12345||DOE^JOHN^||19800101|M|||123 MAIN ST^^METROPOLIS^NY^10001||(555)555-5555|||S|
-                    PV1||O|2000^2012^1||||1234^SMITH^JOHN^MD|||SUR||||ADM|A0""";
 
 
     @BeforeEach
@@ -83,29 +78,28 @@ class BasicMessageParserTest {
 
     @Nested
     class ParserValidMsgTests {
-        HL7Message result;
+        private HL7Message result;
 
 
         @BeforeEach
         void setUp() {
+            String validHl7Message = """
+                    MSH|^~\\&|TEST|TEST|  Lab_System  |Lab_Dept|202603051000||ADT^A04|32143214321|P|2.3
+                    PID|||PI12345||DOE^JOHN^||19800101|M|||123 MAIN ST&APT 4B&METROPOLIS^OH^44123||(555)555-1212~(555)555-1313|||S|
+                    PV1||O|2000^2012^1||||1234^SMITH^JOHN^MD|||SUR||||ADM|A0
+                    """;
             result = parser.parse(validHl7Message, new HL7Message());
         }
 
 
         @Test
-        void parseValidMessageReturnNotNull() {
-            assertNotNull(result);
-        }
-
-
-        @Test
-        void parseValidMessageShouldHaveThreeSegments() {
+        void msgShouldHaveThreeSegments() {
             assertEquals(3, result.getSegments().size());
         }
 
 
         @Test
-        void parseValidMessageShouldHaveCorrectSegmentHeaders() {
+        void msgShouldHaveCorrectSegmentHeaders() {
             final List<String> segHeaders = result
                     .getSegments()
                     .stream()
@@ -120,7 +114,7 @@ class BasicMessageParserTest {
 
 
         @Test
-        void parseValidMessageShouldHavePI12345InPIDThree() {
+        void msgShouldHavePI12345InPIDThree() {
             final var patID = result
                     .getSegments().get(1)
                     .getFieldList().get(3)
@@ -130,8 +124,9 @@ class BasicMessageParserTest {
             assertEquals("PI12345", patID);
         }
 
+
         @Test
-        void parseValidMessageShouldHaveDoeJohninPIDFive() {
+        void msgShouldHaveDoeJohninPIDFive() {
             final var nameField = result
                     .getSegments().get(1)
                     .getFieldList().get(5);
@@ -150,5 +145,149 @@ class BasicMessageParserTest {
             assertEquals("JOHN", fName);
         }
 
+
+        @Test
+        void msgShouldHandleFieldRepetitionsInPIDThirteen() {
+            final var pidPhoneNumField = result
+                    .getSegments().get(1)
+                    .getFieldList().get(13);
+
+            final var phoneOne = pidPhoneNumField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().getFirst();
+
+            final var phoneTwo = pidPhoneNumField
+                    .getRepetitionList().get(1)
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().getFirst();
+
+
+            assertEquals("(555)555-1212", phoneOne);
+            assertEquals("(555)555-1313", phoneTwo);
+        }
+
+
+        @Test
+        void msgShouldTrimAndUpperCaseValuesInMSHFive() {
+            final var mshFiveField = result
+                    .getSegments().getFirst()
+                    .getFieldList().get(4)
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().getFirst();
+
+            assertEquals("LAB_SYSTEM", mshFiveField);
+        }
+
+
+        @Test
+        void msgShouldHandleSubcomponentInPIDEleven() {
+            final var pidAddressField = result
+                    .getSegments().get(1)
+                    .getFieldList().get(11);
+
+            final var pidAddressCompOne = pidAddressField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().getFirst();
+
+            final var pidAddressCompTwo = pidAddressField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().get(1);
+
+            final var pidAddressCompThree = pidAddressField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().get(2);
+
+            assertEquals("123 MAIN ST", pidAddressCompOne);
+            assertEquals("APT 4B", pidAddressCompTwo);
+            assertEquals("METROPOLIS", pidAddressCompThree);
+        }
+
+    }
+
+
+    @Nested
+    class CustomEncodingTests {
+        private HL7Message parsedMsg = new HL7Message();
+
+
+        @BeforeEach
+        void setUp() {
+            parser = new BasicMessageParser();
+            String msgWCustomEnc = """
+                    MSH|:*\\#|SENDING_APP|SENDING_FAC|REC_APP|REC_FAC|202310271030||ADT:A01|12345|P|2.5
+                    EVN||202310271030
+                    PID|1||PATID1234###Facility:MR||Doe:John:Character:Suffix||19800101|M|||123 Main St##Anytown:NY:12345||123-456-7987*126-555-0200|||S||P00123456
+                    PV1|1|I|W4:402:A||||12345:Smith:Jane:Dr|||SUR||||ADM|A0|
+                    """;
+            parsedMsg = parser.parse(msgWCustomEnc, parsedMsg);
+        }
+
+
+        @Test
+        void msgShouldParseRepetitionsWithCustomEnc() {
+            final var pidPhoneNumField = parsedMsg
+                    .getSegments().get(2)
+                    .getFieldList().get(13);
+
+            final var phoneOne = pidPhoneNumField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().getFirst();
+
+            final var phoneTwo = pidPhoneNumField
+                    .getRepetitionList().get(1)
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().getFirst();
+
+
+            assertEquals("123-456-7987", phoneOne);
+            assertEquals("126-555-0200", phoneTwo);
+        }
+
+        @Test
+        void msgShouldParseComponentsWithCustomEnc() {
+            final var mshMsgTypeField = parsedMsg
+                    .getSegments().getFirst()
+                    .getFieldList().get(8);
+
+            final var mshTypeFirstComp = mshMsgTypeField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().getFirst();
+
+            final var mshTypeSecComp = mshMsgTypeField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().get(1)
+                    .getSubcomponentList().getFirst();
+
+            assertEquals("ADT", mshTypeFirstComp);
+            assertEquals("A01", mshTypeSecComp);
+        }
+
+
+        @Test
+        void msgShouldParseSubcomponentsWithCustomEnc() {
+            final var getPidPatIDField = parsedMsg
+                    .getSegments().get(2)
+                    .getFieldList().get(3);
+
+            final var pidPatIdFirstSubcomp = getPidPatIDField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().getFirst();
+
+            final var pidPatIdSecondSubcomp = getPidPatIDField
+                    .getRepetitionList().getFirst()
+                    .getComponentList().getFirst()
+                    .getSubcomponentList().get(3);
+
+            assertEquals("PATID1234", pidPatIdFirstSubcomp);
+            assertEquals("FACILITY", pidPatIdSecondSubcomp);
+        }
     }
 }
