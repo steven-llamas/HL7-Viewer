b03172d5fd6d133931fb886ab2e77a5d2c424eb2
steven L <steven.s.llamas@gmail.com>
2026-04-08 20:32:57 -0500

    moved validation checks to HL7Message class and created public static field

--- src/main/java/hl7Viewer/nonGui/parser/HL7Message.java
+++ src/main/java/hl7Viewer/nonGui/parser/HL7Message.java
@@ -1,10 +1,11 @@
 package hl7Viewer.nonGui.parser;
 
+import javafx.util.Pair;
 import java.util.List;
 
 public class HL7Message {
     private List<HL7Segment>  segments;
-
+    public static final String NORMAL_ENCODING = "|^~\\&";
 
     public void setSegments(List<HL7Segment> segments) {
         this.segments = segments;
@@ -23,7 +24,26 @@ public class HL7Message {
 
 
     public static String sanitizeEnterChar(String message) {
-        message = message.replaceAll("\\R","\r");
-        return message;
+        return message.replaceAll("\\R","\r");
+    }
+
+    public static Pair<Boolean,String> isValidHl7Message(final String message) {
+        var isValid = false;
+        String errorMsg = "Invalid Message: ";
+
+        if (message.isEmpty())
+                return new Pair<>(isValid, errorMsg + "message cannot be empty");
+
+        if(!message.substring(0,3).toUpperCase().contains("MSH"))
+                return new Pair<>(isValid, errorMsg +
+                        "Message doesn't contain MSH as first segment");
+
+        if((message.length() < 4))
+                 return new Pair<>(isValid, errorMsg +
+                         "Message length is too short to parse");
+
+        isValid = true;
+        errorMsg = "";
+        return new Pair<>(isValid, errorMsg);
     }
 }
