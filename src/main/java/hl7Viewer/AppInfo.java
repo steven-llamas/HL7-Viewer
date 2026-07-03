41d5f2d569ff38dcb281d4a7b416e3b934206272
steven L <steven.s.llamas@gmail.com>
2026-07-03 16:09:25 -0500

    Bumped version to 1.0.4 to satisfy jpackage version format requirement

--- src/main/java/hl7Viewer/AppInfo.java
+++ src/main/java/hl7Viewer/AppInfo.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.net.URISyntaxException;
 
 public final class AppInfo {
-    public static final String VERSION = "0.4";
+    public static final String VERSION = "1.0.4";
 
     public static final String BUILD_TYPE = isJar() ? "Release" : "Debug";
 
