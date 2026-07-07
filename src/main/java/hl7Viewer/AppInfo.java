630baa96240ae7a7384267e18e5b14dd8250e2ac
steven L <steven.s.llamas@gmail.com>
2026-07-07 13:43:51 -0500

    Bumped version to 1.1.0

--- src/main/java/hl7Viewer/AppInfo.java
+++ src/main/java/hl7Viewer/AppInfo.java
@@ -6,7 +6,7 @@ import java.net.URISyntaxException;
 public final class AppInfo {
     public static final String APP_NAME = "HL7 Viewer";
 
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.1.0";
 
     public static final String BUILD_TYPE = isJar() ? "Release" : "Debug";
 
